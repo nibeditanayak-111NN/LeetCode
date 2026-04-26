8bcfa9ea9ccf50b40bada626c38f04d15edf0a8c
Nibedita Nayak <nibeditanayak474@gmail.com>
2026-04-26 18:00:18 +0530

    Time: 12 ms (5.63%) | Memory: 12.4 MB (25.42%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -6,23 +6,19 @@ class Solution:
         i = 0
         n = len(s)
         
-        # 1. Skip whitespace
         while i < n and s[i] == ' ':
             i += 1
         
-        # 2. Check sign
         sign = 1
         if i < n and (s[i] == '+' or s[i] == '-'):
             if s[i] == '-':
                 sign = -1
             i += 1
         
-        # 3. Convert digits
         num = 0
         while i < n and s[i].isdigit():
             digit = int(s[i])
             
-            # 4. Check overflow BEFORE adding digit
             if num > (INT_MAX - digit) // 10:
                 return INT_MAX if sign == 1 else INT_MIN
             
