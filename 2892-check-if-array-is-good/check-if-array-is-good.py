3f016c883a5eee39a8a53ad597c601e215812e6b
Nibedita Nayak <nibeditanayak474@gmail.com>
2026-05-14 15:54:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (0.00%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isGood(self, nums):
+        n = max(nums)
+
+        if len(nums) != n + 1:
+            return False
+
+        nums.sort()
+
+        for i in range(1, n):
+            if nums[i - 1] != i:
+                return False
+
+        return nums[-1] == n and nums[-2] == n
