87387161d9bd574a1085e2540444bc91c76e0b95
Nibedita Nayak <nibeditanayak474@gmail.com>
2026-05-25 21:01:18 +0530

    Time: 290 ms (56.10%) | Memory: 16.1 MB (82.93%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Solution:
+    def canReach(self, s, minJump, maxJump):
+
+        n = len(s)
+        queue = deque([0])
+        farthest = 0
+
+        while queue:
+            i = queue.popleft()
+
+            start = max(i + minJump, farthest)
+            end = min(i + maxJump, n - 1)
+
+            for j in range(start, end + 1):
+                if s[j] == '0':
+                    if j == n - 1:
+                        return True
+                    queue.append(j)
+
+            farthest = end + 1
+
+        return n == 1
